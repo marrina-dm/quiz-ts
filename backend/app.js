c0804c5047553a9b658c9ad3534dc1ebba02872e
marrina-dm <marinadmitrieva802@gmail.com>
2024-05-04 22:08:10 +0300

    part 4

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const testRoutes = require('./routes/test.routes');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +12,6 @@ app.use(cors());
 app.use("/api", authRoutes);
 app.use("/api/tests", testRoutes);
 
-app.listen('3000', () =>
+app.listen(port, () =>
     console.log(`Server started`)
 )
